e5aeba75e6630436cd3fbf047b3ade858004d9d6
86135 <59655694@qq.com>
2020-12-02 00:44:40 +0800

    pratice homework one

---
+++ SendMoney/select_money.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import money
+
+
+def select_money():
+    print(f'现在存款有{money.saved_money}')
---
+++ SendMoney/send_money.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import money
+
+
+def send_money():
+    print('发工资啦！')
+    money.saved_money = money.saved_money + 1000
